2d18d58805b1e8638f08beef07a5df541ae3b8b9
James Loy <jayloy98@gmail.com>
2022-09-23 09:49:58 -0500

    Updated files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const display1El = document.querySelector('.display-1')
 const display2El = document.querySelector('.display-2')
 const numbersEl = document.querySelector('.number')
@@ -50,5 +48,86 @@ function clearVar(name = ''){
   tempResultEl.innerText = result
 }
 
+function mathOperation(){
+  if(lastOperation === 'x'){
+    result = parseFloat(result) * parseFloat(dis2Num)
+  }else if(lastOperation === '+'){
+    result = parseFloat(result) + parseFloat(dis2Num)
+  }else if(lastOperation === '-'){
+    result = parseFloat(result) - parseFloat(dis2Num)
+  } else if(lastOperation === '/'){
+    result = parseFloat(result) / parseFloat(dis2Num)
+  }else if(lastOperation === '%'){
+    result = parseFloat(result) % parseFloat(dis2Num)
+  }
+}
+
+equalEl.addEventListener('click', () => {
+  if (!dis2Num || !dis1Num) return
+  haveDot = false
+  mathOperation()
+  clearVar()
+  display2El.innerText = result
+  tempResultEl.innerText = ''
+  dis2Num = result
+  dis1Num = ''
+})
+
+clearAllEl.addEventListener('click', () => {
+  dis1Num =''
+  dis2Num = ''
+  display1El.innerText = ''
+  display2El.innerText = ''
+  result = ''
+  tempResultEl.innerText = ''
+})
+
+clearLastEl.addEventListener('click', () => {
+  display2El.innerText = ''
+  dis2Num = ''
+})
+
+window.addEventListener('keydown', (e) => {
+  if(
+    e.key === '0' || 
+    e.key === '1' || 
+    e.key === '2' ||
+    e.key === '3' || 
+    e.key === '4' || 
+    e.key === '5' || 
+    e.key === '6' || 
+    e.key === '7' || 
+    e.key === '8' || 
+    e.key === '9' || 
+    e.key === '.')
+    {
+    clickButtonEl(e.key)
+  } else if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === '%'){
+    clickOperation(e.key)
+  }else if (e.key === '*'){
+    clickOperation('x')
+  }else if (e.key === 'Enter' || e.key === '='){
+    clickEqual()
+  }
+})
+
+function clickButtonEl(key) {
+  numbersEl.forEach((button) => {
+    if(button.innerText === key){
+      button.click()
+    }
+  })
+}
 
 
+function clickOperation(key){
+  operationEl.forEach((operation) => {
+    if (operation.innerText === key) {
+      operation.click()
+    }
+  })
+}
+
+function clickEqual() {
+  equalEl.click()
+}
